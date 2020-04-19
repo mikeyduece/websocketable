1eca6011b8885c555c5827b4eceeb1d9f5572dc1
Mike Heft <mikeheft@gmail.com>
2020-04-19 14:22:31 -0600

    Refactor template path

--- lib/generators/websocketable/install_generator.rb
+++ lib/generators/websocketable/install_generator.rb
@@ -1,6 +1,6 @@
 module Websocketable
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../templates", __FILE__)
+    source_root File.expand_path("../../templates", __FILE__)
 
     def copy_initializer
       template('initializer.rb', 'config/initializers/webscocketable.rb')
